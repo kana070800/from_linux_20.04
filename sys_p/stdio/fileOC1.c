f96b3c71f938c52a386e9556f95ca73051431a10
kana070800 <kanawer070800@gmail.com>
2025-10-03 21:40:54 +0900

    stdio test

---
+++ sys_p/stdio/fileOC1.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	FILE* rfp, * wfp;
+	int c;
+
+	if ((rfp = fopen("hello.txt", "r")) == NULL){
+		perror("fopen: hello.txt");
+		exit(1);
+	}
+	if ((wfp = fopen("hello.out", "w")) == NULL){
+		perror("fopen: hello.txt");
+		exit(1);
+	}
+
+	while ((c = fgetc(rfp)) != EOF){
+		fputc(c, wfp);
+	}
+
+	fclose(rfp);
+	fclose(wfp);
+
+	return 0;
+}
