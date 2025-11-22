0441574904f11896724a976bf439b28364aafce5
kana070800 <kanawer070800@gmail.com>
2025-11-22 10:59:16 +0900

    test

---
+++ sys_p/file_io/fcntl.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+	int fd, flags;
+
+	fd = open("linux.txt", O_RDWR);
+	if (fd == -1){
+		perror("open");
+		exit(1);
+	}
+
+	if ((flags = fcntl(fd, F_GETFL)) == -1){
+		perror("fcntl");
+		exit(1);
+	}
+
+	flags |= O_APPEND;
+
+	if (fcntl(fd, F_SETFL, flags) == -1){
+		perror("fcntl");
+		exit(1);
+	}
+
+	if (write(fd, "KOREATECH\n", 9) != 9)
+		perror("write");
+	close(fd);
+
+	return 0;
+}
